5de4baec5221115cf3232ed65ed5b3933bece9c5
Anthony <anmccbusiness@gmail.com>
2018-07-22 13:27:03 -0700

    Implementing Models and Migrations (Not Complete)

---
+++ CarRepairTracker/Models/Part.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRepairTracker.Models
+{
+    /// <summary>
+    /// Represents a car part.
+    /// </summary>
+    public class Part
+    {
+        [Key] // Makes the property the PK
+        // If PK property is an integer, it will be an identity column by default.
+        public int PartID { get; set; }
+
+        [Required] // You can use parentheses after required, but it is not required
+        [StringLength(40)]
+        public string Name { get; set; }
+
+        [Range(0, 100000)] //Inclusive??
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        // TODO: Add description
+    }
+}
